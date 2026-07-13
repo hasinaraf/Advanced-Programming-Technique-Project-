ccbd91ef3707f15e6bc34a442eb7d038ac915f9b
Hasin Araf <hasinaraff@gmail.com>
2026-07-13 10:16:38 +0100

    online resource: https://flask-sqlalchemy.readthedocs.io/en/stable/quickstart/#check-the-sqlalchemy-documentation -- SQLALCHEMY_DATABASE_URI setup

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 # Flask app for the issue tracker
 
 from flask import Flask, jsonify
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 # create Flask app
 app = Flask(__name__)
 
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
 
 # home route to check if the website is running
 @app.route("/")
@@ -20,6 +28,10 @@ def health_check():
         "company": "GoCar Ireland",
         "message": "API is running"
     })
+
+# create database tables if they exist in the code
+with app.app_context():
+    db.create_all()
 
 
 # run Flask app
